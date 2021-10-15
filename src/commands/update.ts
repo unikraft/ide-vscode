c7bb36c384644f183b68cce752a32cd4bf12805a
Adina Smeu <adina.smeu@gmail.com>
2021-10-15 17:04:38 +0300

    Add update command

---
+++ src/commands/update.ts
@@ -0,0 +1,22 @@
+import { OutputChannel, StatusBarItem } from 'vscode';
+import { Command } from './Command';
+import { getGithubToken } from './utils';
+
+export async function kraftUpdate(
+    kraftChannel: OutputChannel,
+    kraftStatusBarItem: StatusBarItem
+) {
+    const command = new Command(
+        `kraft list update`,
+        {
+            env: Object.assign(
+                process.env,
+                { 'UK_KRAFT_GITHUB_TOKEN': getGithubToken() })
+        },
+        'Finished running kraft list update.'
+    );
+
+    kraftStatusBarItem.text = 'kraft list update...';
+
+    command.execute(kraftChannel, kraftStatusBarItem);
+}
